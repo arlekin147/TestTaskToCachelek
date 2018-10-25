10a1801a7db45058b7d66c3eb9fd2ccb23b7eb7a
Sergey Kharitonov <h.serge2@gmail.com>
2018-10-26 02:49:52 +0300

    Initial commit

---
+++ WebApplication4/Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+
+
+namespace RazorPages
+{
+    public class IndexModel : PageModel
+    {
+
+        private static WebClient client = new WebClient();
+
+        [BindProperty]
+        public string code { get; set; }
+        [BindProperty]
+        public string message { get; set; }
+
+        public void OnGet()
+        {
+            if(code != null && message != null)
+            {
+                
+            }
+        }
+        public IActionResult OnPost()
+        {
+            var parameters = new NameValueCollection { { "token", "ajs2vpgniz8d4kkox4wbghmdnodjxs" }, { "user", code }, { "message", message }
+                };
+            Console.WriteLine(code + " " + message + " !!!! ");
+            client.UploadValues("https://api.pushover.net/1/messages.json", parameters);
+            return Page();
+
+        }
+    }
+}
